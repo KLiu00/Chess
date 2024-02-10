8acfaa1f8cd140dc244666f44b5d59c5eaed1ba2
KLiu00 <98055268+KLiu00@users.noreply.github.com>
2024-02-10 13:43:59 +0000

    Created move class

---
+++ Move.py
@@ -0,0 +1,9 @@
+from Pieces.IPiece import IPiece
+
+class Move:
+
+    def __init__(self, startPosition: int, endPosition: int, pieceMoved: IPiece, capturedPiece: IPiece) -> None:
+        self.startPosition = startPosition
+        self.endPosition = endPosition
+        self.pieceMoved = pieceMoved
+        self.capturedPiece = capturedPiece
