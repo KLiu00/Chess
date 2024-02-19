1f845c374a95221b3df80ebb618584b9625197d4
KLiu00 <98055268+KLiu00@users.noreply.github.com>
2024-02-19 17:41:32 +0000

    Created board index to RankFile notation function

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from Engine import ChessEngine as Chess
 from Enums.PieceEnum import PieceEnum
 from Enums.SideEnum import SideEnum
 
+def indexToRF(index: int ) -> str:
+    files = ["a","b","c","d","e","f","g","h"]
+    return f"{files[index%8]}{8-index//8}"
+
 if __name__ == '__main__':
     instance = Chess()
     print(instance.displayBoard())
