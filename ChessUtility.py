458ff6f37e1b989a14dc3b03a6fe56a14c94fae1
KLiu00 <98055268+KLiu00@users.noreply.github.com>
2024-03-06 18:04:44 +0000

    Made rankfile to index conversion, and made the game playable

--- ChessUtility.py
+++ ChessUtility.py
@@ -1,3 +1,8 @@
+files = ["a", "b", "c", "d", "e", "f", "g", "h"]
 def indexToRF(index: int) -> str:
-    files = ["a", "b", "c", "d", "e", "f", "g", "h"]
     return f"{files[index%8]}{8-index//8}"
+
+def rfToIndex(rf: str) -> int:
+    rank = rf[1]
+    file = rf[0]
+    return files.index(file) + 64 - 8 * int(rank)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Engine import ChessEngine as Chess
-from ChessUtility import indexToRF
+from ChessUtility import indexToRF, rfToIndex
 from Enums.PieceEnum import PieceEnum
 from Enums.SideEnum import SideEnum
 
@@ -9,18 +9,27 @@ if __name__ == '__main__':
     while not instance.checkmated:
         print(instance.displayBoard())
         all_moves = instance.generate_all_moves()
+        rf_moves = []
         total = 0
         for boardIndex, move_set in all_moves:
             for move in move_set:
                 total += 1 
+                rf_moves.append(f"{indexToRF(boardIndex)}{indexToRF(move)}")
                 print(f"{indexToRF(boardIndex)}{indexToRF(move)}", end=" ")
         print("\nTotal amount of moves:" , total)
-                
-        fromMake = int(input("From: "))
-        toMake = int(input("to: "))
 
-        if fromMake == -1 or toMake == -1:
+        fromMake = input("From: ")
+        toMake = input("to: ")
+
+        if fromMake == "-1" or toMake == "-1":
             instance.unmakeMove()
             continue
 
-        instance.makeMove(fromMake, toMake)
+        # Check if move in allowed moves list
+        if fromMake + toMake not in rf_moves:
+            # if not in the list, ask for move again by skipping make move logic
+            continue
+        fromIndex = rfToIndex(fromMake)
+        toIndex = rfToIndex(toMake)
+
+        instance.makeMove(fromIndex, toIndex)
