13365888db30bc0e7d399f812a94f83ce907129c
KLiu00 <kelvinyliu@gmail.com>
2024-03-07 09:45:33 +0000

    Improved Move class to include more information which is required for En Passant.

--- Move.py
+++ Move.py
@@ -2,8 +2,9 @@ from Pieces.BasePiece import IPiece
 
 class Move:
 
-    def __init__(self, startPosition: int, endPosition: int, pieceMoved: IPiece, capturedPiece: IPiece, enPassant = False) -> None:
+    def __init__(self, startPosition: int, endPosition: int, pieceMoved: IPiece, capturedPiece: IPiece, enPassant = False, capturedPiecePosition: int = 0) -> None:
+        self.pieceMoved = pieceMoved
         self.startPosition = startPosition
         self.endPosition = endPosition
-        self.pieceMoved = pieceMoved
-        self.capturedPiece = capturedPiece
+        self.capturedPieceMoved = capturedPiece
+        self.capturedPiecePosition = capturedPiecePosition if capturedPiecePosition != 0 else endPosition
