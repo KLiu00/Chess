68c9ec3b5ced5644e126f4f7f036a8f3e9e93cb9
KLiu00 <98055268+KLiu00@users.noreply.github.com>
2024-03-09 14:35:42 +0000

    Reformatted Move class and created inherited CastleMove class

--- Move.py
+++ Move.py
@@ -2,9 +2,16 @@ from Pieces.BasePiece import IPiece
 
 class Move:
 
-    def __init__(self, startPosition: int, endPosition: int, pieceMoved: IPiece, capturedPiece: IPiece, enPassant = False, capturedPiecePosition: int = 0) -> None:
-        self.pieceMoved = pieceMoved
+    def __init__(self, startPosition: int, endPosition: int, pieceMoved: IPiece, capturedPiece: IPiece, capturedPiecePosition: int = 0) -> None:
         self.startPosition = startPosition
         self.endPosition = endPosition
+        self.pieceMoved = pieceMoved
         self.capturedPieceMoved = capturedPiece
         self.capturedPiecePosition = capturedPiecePosition if capturedPiecePosition != 0 else endPosition
+
+class CastleMove(Move):
+    def __init__(self, king_position: int, rook_position: int, king_piece: IPiece, rook_piece: IPiece, king_end_position, rook_end_position) -> None:
+        super().__init__(king_position, king_end_position, king_piece, None)
+        self.rook_piece = rook_piece
+        self.rook_start_position = rook_position
+        self.rook_end_position = rook_end_position
